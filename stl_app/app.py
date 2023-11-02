31c7e6d765ac79090a0e73c0a17fb03452ee9495
pink <66056077@kmitl.ac.th>
2023-11-02 19:16:19 +0700

    revise app.py

--- stl_app/app.py
+++ stl_app/app.py
@@ -7,8 +7,11 @@ from st_pages import Page, show_pages, add_page_title
 
 show_pages([
     Page("pages/05_iris_data/app.py", "IRIS Data"),
-    Page("pages/05_iris_data/map.py", "Map Layout"),
+    Page("pages/06_iris_ml/app_ml.py", "IRIS ML"),
+    Page("pages/06_iris_ml/app_st.py", "IRIS ST")
 ])
 
 add_page_title()
 
+st.markdown('สวัสดี! *Streamlit*')
+st.title('IRIS Data')
